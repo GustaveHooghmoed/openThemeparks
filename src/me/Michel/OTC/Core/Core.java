336c96e578791867d33df9fa991268b9d2385937
IKEA <michelarts200015@gmail.com>
2017-10-22 00:11:17 +0200

    Updated build.gradle

--- src/me/Michel/OTC/Core/Core.java
+++ src/me/Michel/OTC/Core/Core.java
@@ -3,6 +3,7 @@ package me.Michel.OTC.Core;
 import me.Michel.OTC.Modules.ModuleManager;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+
 public class Core extends JavaPlugin {
 
 
@@ -11,12 +12,14 @@ public class Core extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        plugin = this;
         this.moduleManager = new ModuleManager();
+
     }
 
     @Override
     public void onDisable() {
-
+        plugin = null;
     }
 
     public static Plugin getPlugin() {
